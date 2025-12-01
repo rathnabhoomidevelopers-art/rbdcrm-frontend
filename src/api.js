13a28a4a4ccf48d1e13054c2a7d7b79ccc11fd4c
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2025-12-01 17:11:52 +0530

    update the REACT_API_BASE_URL

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:3000";
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// Attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -15,14 +14,11 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// Optional: handle 401 globally
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response && err.response.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("role");
-      localStorage.removeItem("username");
+    if (err.response?.status === 401) {
+      localStorage.clear();
       window.location.href = "/userlogin";
     }
     return Promise.reject(err);
